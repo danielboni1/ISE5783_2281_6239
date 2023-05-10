9fc097edb6df2d5e671642481a674792b4b8555f
אוריאל מנגיסטו <umangist@g.jct.ac.il>
2023-05-10 19:38:50 +0300

    Added ambient light and image writer

--- unitTests/renderer/ImageWriterTests.java
+++ unitTests/renderer/ImageWriterTests.java
@@ -10,11 +10,25 @@ class ImageWriterTests {
     ImageWriter imageWriter = new ImageWriter("first", 800,500);
     @Test
     void testWriteToImage() {
+
+        for (int i = 0;i<800;i++) {
+            for (int j = 0; j < 500; j++) {
+                imageWriter.writePixel(i, j, new Color(64, 86, 255));
+            }
+        }
+        for (int i = 0;i<800;i++) {
+            for (int j = 0; j < 500; j+=50) {
+                imageWriter.writePixel(i, j, new Color(255, 45, 37));
+            }
+        }
+        for (int i = 0;i<800;i+=50) {
+            for (int j = 0; j < 500; j++) {
+                imageWriter.writePixel(i, j, new Color(255, 45, 37));
+            }
+        }
         imageWriter.writeToImage();
     }
 
-    @Test
-    void testWritePixel() {
-        imageWriter.writePixel(10,16, new Color(255,0,0));
-    }
+
+
 }
