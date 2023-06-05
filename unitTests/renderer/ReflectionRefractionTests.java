59cd6eceebca4942cf965ac855cfea3a442884f2
דניאל בוני <danielboni058@gmail.com>
2023-06-05 15:16:20 +0300

    bonus stage 7

--- unitTests/renderer/ReflectionRefractionTests.java
+++ unitTests/renderer/ReflectionRefractionTests.java
@@ -5,6 +5,9 @@ package renderer;
 
 import static java.awt.Color.*;
 
+import geometries.Polygon;
+import lighting.DirectionalLight;
+import lighting.PointLight;
 import org.junit.jupiter.api.Test;
 
 import geometries.Sphere;
@@ -14,6 +17,8 @@ import lighting.SpotLight;
 import primitives.*;
 import renderer.*;
 import scene.Scene;
+
+import java.util.List;
 
 /** Tests for reflection and transparency functionality, test for partial
  * shadows
@@ -97,11 +102,73 @@ public class ReflectionRefractionTests {
 
       scene.lights.add(new SpotLight(new Color(700, 400, 400), new Point(60, 50, 0), new Vector(0, 0, -1)) //
          .setKl(4E-5).setKq(2E-7));
-
       ImageWriter imageWriter = new ImageWriter("refractionShadow", 600, 600);
       camera.setImageWriter(imageWriter) //
          .setRayTracer(new RayTracerBasic(scene)) //
          .renderImage() //
          .writeToImage();
+   }
+   @Test
+
+   public void allefectTestBounos() {
+      Camera camera = new Camera(new Point(0, 0, -1000), new Vector(0, 0, 1), new Vector(0, -1, 0)) .setVPSize(200, 200).setVPDistance(1000);
+      scene.setAmbientLight(new AmbientLight(new Color(WHITE), 0.15)).background.add(new Color(white));
+
+      scene.geometries.add( //
+              new Triangle( new Point(-55, 10, 115), new Point(-25, 49, 115), new Point(-85, 49, 115))
+                      .setEmission(new Color(76, 3, 19)), //
+              new Sphere( 50, new Point(70, -70, 0))
+                      .setEmission(new Color(200, 200, 0)),
+              new Sphere(30, new Point(70, -70, -10))
+                      .setEmission(new Color(java.awt.Color.orange)),
+              new Sphere(20, new Point(70, -70, -30))
+                      .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60))
+                      .setEmission(new Color(java.awt.Color.RED)),
+              new Sphere( 20, new Point(-70, -60, -30))
+                      .setEmission(new Color(java.awt.Color.darkGray))
+                      .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60)),
+              new Sphere( 20, new Point(-60, -50, -30))
+                      .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60))
+                      .setEmission(new Color(java.awt.Color.darkGray)),
+              new Sphere( 20, new Point(-40, -65, -30))
+                      .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60))
+                      .setEmission(new Color(java.awt.Color.darkGray)),
+
+              new Polygon(new Point(25, 110, 115), new Point(35, 110, 115), new Point(35, 50, 115), new Point(25, 50, 115))
+                      .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60))
+                      .setEmission(new Color(17, 11, 5)),
+
+              new Triangle( new Point(30, -20, 115), new Point(15, 50, 115), new Point(45, 50, 115))
+              .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60))
+                      .setEmission(new Color(0, 150, 0)),//
+              new Polygon(new Point(-80, 110, 115), new Point(-30, 110, 115), new Point(-30, 50, 115), new Point(-80, 50, 115))
+                      .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60))
+                      .setEmission(new Color(240, 250, 240))
+              , new Triangle(new Point(-30, -10, 115), new Point(-30, -14, 113), new Point(-25, -12, 114))
+                      .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60))
+                      .setEmission(new Color(54, 40, 30))
+              , new Triangle(new Point(-27, -10, 115), new Point(-27, -14, 113), new Point(-22, -12, 114))
+                      .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60))
+                      .setEmission((new Color(54, 40, 30)))
+              , new Triangle(new Point(-20, -15, 101), new Point(-20, -19, 99), new Point(-15, -17, 100))
+                              .setEmission(new Color(54, 40, 30))
+                      .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60))
+              , new Triangle(new Point(-17, -15, 101), new Point(-17, -19, 99), new Point(-12, -17, 100))
+                      .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60))
+                      .setEmission(new Color(54, 40, 30))
+
+      );
+
+
+      scene.lights.add(new SpotLight(new Color(yellow), new Point(60, 50, 0), new Vector(0, 0, -1)) //
+              .setKl(4E-5).setKq(2E-7));
+
+
+      ImageWriter imageWriter = new ImageWriter("bonus", 600, 600);
+      camera.setImageWriter(imageWriter) //
+              .setRayTracer(new RayTracerBasic(scene)) //
+              .renderImage() //
+              .writeToImage();
+
    }
 }
